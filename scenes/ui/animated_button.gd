c4962bf326aafead94908926e2a67dd14f0c5732
Ross Karchner <ross@karchner.com>
2023-05-29 15:01:36 -0400

    updated levels

---
+++ scenes/ui/animated_button.gd
@@ -0,0 +1,22 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var mouse_entered = func():
+		$HoverAnimationPlayer.play("hover")
+		
+	var mouse_exited = func():
+		$HoverAnimationPlayer.play_backwards("hover")
+		
+	var clicked = func():
+		$ClickAnimationPlayer.play("click")
+		
+	connect("mouse_entered", mouse_entered)
+	connect("mouse_entered", mouse_exited)
+	connect("pressed", clicked)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pivot_offset = custom_minimum_size /2

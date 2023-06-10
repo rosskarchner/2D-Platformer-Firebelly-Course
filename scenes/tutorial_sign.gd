b7178fc80b66be125b60bb367f508eca001cb606
Ross Karchner <ross@karchner.com>
2023-06-09 23:49:25 -0400

    add tutorial signs

---
+++ scenes/tutorial_sign.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+@export_multiline var text = "Change me"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	%Label.text = text
+	$PanelContainer.visible = false
+	$Area2D.connect("area_entered", func(_collider):
+		$PanelContainer.visible = true
+		$Sprite2D.frame = 1
+		print_debug("entered@")
+	)
+	$Area2D.connect("area_exited", func(_collider):
+		$PanelContainer.visible = false
+		$Sprite2D.frame = 0
+		pass
+	)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+

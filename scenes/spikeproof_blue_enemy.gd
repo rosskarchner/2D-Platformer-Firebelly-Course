a5720d54bb790db815745b1ceee840b09a2d8a34
Ross Karchner <ross@karchner.com>
2023-07-07 18:15:42 -0400

    new enemy, updated levels

---
+++ scenes/spikeproof_blue_enemy.gd
@@ -0,0 +1,70 @@
+extends CharacterBody2D
+
+enum Direction{RIGHT, LEFT}
+
+@export var startDirection: Direction = Direction.RIGHT
+var isSpawning = true
+
+var enemyDeathScene = preload("res://scenes/enemy_death.tscn")
+var maxSpeed = 25
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var directionLookup ={Direction.LEFT: Vector2.LEFT, Direction.RIGHT: Vector2.RIGHT}
+@onready var direction = directionLookup[startDirection]
+
+func _ready():
+	#$GoalDetector.connect("area_entered", self.on_goal_entered)
+	$HitBoxArea.connect("area_entered", self.on_hitbox_enter)
+
+func _physics_process(delta):
+	if isSpawning:
+		return
+	if scale.x == direction.x:
+		update_visual_direction()
+		
+	velocity.x = (direction * maxSpeed).x
+	if !is_on_floor(): 
+		velocity.y += gravity * delta
+		
+	move_and_slide()
+	if is_on_floor():
+	
+		if !$Visuals/CliffDetector.is_colliding():
+			jump()
+			
+		if $Visuals/WallDetector.is_colliding() and !$"Visuals/Can Jump Detecter".is_colliding():
+			jump()
+			
+		elif $Visuals/WallDetector.is_colliding():
+			change_direction()
+
+
+func update_visual_direction():
+	var visuals = $Visuals
+	if direction.x > 0:
+		visuals.scale.x = -1
+	else: 
+		visuals.scale.x = 1
+
+
+
+func change_direction():
+	direction *= -1
+	update_visual_direction()
+	velocity.y = -300
+
+func jump():
+
+	velocity.y = -400
+
+
+func on_hitbox_enter(_area2d):
+	var scoreKeeper = $"/root/ScoreKeeper"
+	scoreKeeper.add_points(25)
+	$"/root/Helpers".apply_camera_shake(1.0)
+	var myDeathScene = enemyDeathScene.instantiate()
+	myDeathScene.global_position = global_position
+	if velocity.x > 0:
+		myDeathScene.scale = Vector2(-1,0)
+	add_sibling(myDeathScene)
+	queue_free()

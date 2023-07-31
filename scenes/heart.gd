9767f401e674b02655676cec02e069a4ff2a9478
Ross Karchner <ross@karchner.com>
2023-07-31 17:53:32 -0400

    catchup

--- scenes/heart.gd
+++ scenes/heart.gd
@@ -1,12 +1,9 @@
 extends Node2D
-
-@onready var baseLevel = get_tree().get_nodes_in_group("base_level")[0]
-
-func _ready():
-	baseLevel.update_coin_total()
 	
 func on_area_entered(_area2d):
 	$AnimationPlayer.play("pickup")
+	$RandomAudioStreamPlayer.play()
+	$RandomAudioStreamPlayer2.play()
 	$Area2D/CollisionShape2D.set_deferred("disabled", true)
 	ScoreKeeper.add_life()
 
